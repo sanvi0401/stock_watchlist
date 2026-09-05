8e048df1316f18255e22504887f13208bada482f
sanvi0401 <sd4912@srmist.edu.in>
2026-09-05 19:32:15 +0530

    Route forgot password to authenticator recovery

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Suspense, lazy } from 'react'
 import { Navigate, Route, Routes, useParams, useSearchParams } from 'react-router-dom'
 import AppShell from './layouts/AppShell'
-import { ForgotPage, LandingPage, LoginPage, ResetPage, SignUpPage } from './pages/Public'
+import { LandingPage, LoginPage, ResetPage, SignUpPage } from './pages/Public'
 import { RecoverPasswordPage, SecurityPage } from './pages/Security'
 import Onboarding from './pages/Onboarding'
 import OverviewPage from './pages/Overview'
@@ -19,7 +19,7 @@ function DiscoverRoute() { const [params] = useSearchParams(); const q = params.
 export default function App() {
   return <Routes>
     <Route path="/" element={<LandingPage />} /><Route path="/login" element={<LoginPage />} /><Route path="/signup" element={<SignUpPage />} />
-    <Route path="/forgot-password" element={<ForgotPage />} /><Route path="/recover-password" element={<RecoverPasswordPage />} /><Route path="/reset-password" element={<ResetPage />} />
+    <Route path="/forgot-password" element={<RecoverPasswordPage />} /><Route path="/recover-password" element={<RecoverPasswordPage />} /><Route path="/reset-password" element={<ResetPage />} />
     <Route path="/onboarding" element={<Guard><Onboarding /></Guard>} />
     <Route path="/app" element={<Guard><AppShell /></Guard>}><Route index element={<Navigate to="/app/overview" replace />} /><Route path="overview" element={<OverviewPage />} /><Route path="watchlists" element={<WatchlistsPage />} /><Route path="watchlists/:id" element={<WatchlistDetailPage />} /><Route path="stocks/:symbol" element={<StockRoute />} /><Route path="discover" element={<DiscoverRoute />} /><Route path="history" element={<HistoryPage />} /><Route path="settings" element={<SettingsPage />} /><Route path="security" element={<SecurityPage />} /><Route path="profile" element={<Navigate to="/app/settings" replace />} /><Route path="preferences" element={<Navigate to="/app/settings" replace />} /><Route path="notifications" element={<NotificationsPage />} /></Route>
     <Route path="*" element={<NotFoundPage />} />
